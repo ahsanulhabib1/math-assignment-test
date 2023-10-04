abcc2e191705c5bef6ded1ced50ba25aeb3e659c
ahsanulhabib1 <89745356+ahsanulhabib1@users.noreply.github.com>
2023-10-04 20:24:04 +0600

    Create popup.js

---
+++ scripts/popup.js
@@ -0,0 +1,11 @@
+function togglePopup(popupId) {
+  const popup = document.getElementById(popupId);
+  popup.classList.toggle("open");
+}
+
+var form = document.getElementById("equation-popup");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
