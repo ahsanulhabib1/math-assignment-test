f8ef2c9d1b5db951d7f77d220aae27dd5d781a44
ahsanulhabib1 <89745356+ahsanulhabib1@users.noreply.github.com>
2023-10-15 18:53:00 +0600

    Updated Popup JS

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,9 +3,64 @@ function togglePopup(popupId) {
   popup.classList.toggle("open");
 }
 
-var form = document.getElementById("equation-popup");
+var form1 = document.getElementById("equation-popup");
 
-form.addEventListener("submit", function (e) {
+form1.addEventListener("submit", function (e) {
   e.preventDefault();
   
 });
+
+var form2 = document.getElementById("slope-popup");
+
+form2.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+
+var form3 = document.getElementById("intercept-popup");
+
+form3.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+
+
+var form4 = document.getElementById("twoline-popup");
+
+form4.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+
+var form5 = document.getElementById("distance-popup");
+
+form5.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+
+var form6 = document.getElementById("concurrency-popup");
+
+form6.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+var form7 = document.getElementById("area-popup");
+
+form7.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+var form8 = document.getElementById("parellelism-popup");
+
+form8.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+var form9 = document.getElementById("polar-popup");
+
+form9.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+});
+
